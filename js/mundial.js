f19278ee43338cee63ec9ba91651aa8e43d1cbed
tomeee <tomeeee.1414@gmail.com>
2022-05-04 23:36:13 -0300

    arreglos para terminar la eleccion de paises hasta la recta final

--- js/mundial.js
+++ js/mundial.js
@@ -8,6 +8,7 @@ let findSelected = (i) => {
     let input2=nodos[3];
     p.textContent=selected;
     input.removeAttribute("hidden", "");
+    input2.removeAttribute("hidden", "");
     input.setAttribute("id",selected);
     input.setAttribute("value",selected);
     input2.setAttribute("value",selected);
@@ -69,7 +70,8 @@ for (let j=1;j<=8; j++){
             let p=nodos[1];
             let input=nodos[2];
             p.textContent=selected;
-            img.setAttribute("src",`files/selected.png` )
+            img.setAttribute("src",`files/selected.png` );
+            input.setAttribute("value",selected);
             console.log(selected);
 
 
@@ -77,4 +79,54 @@ for (let j=1;j<=8; j++){
     })
 }
 
+for(let n=1;n<=8;n++){
+    let llave= document.getElementById(`llave${n}`);
+    llave.addEventListener('click',(e)=> {
+
+        if(e.target && e.target.tagName ==='INPUT'){
+            console.log(e.target.name);
+            let cuarto= document.getElementById(`${e.target.name}`).children;
+            let img=cuarto[0];
+            let p =cuarto[1];
+            let input=cuarto[2]
+            input.setAttribute("value",e.target.value);
+            p.textContent=e.target.value;
+
+        }
+    });
+}
+for(let m=1;m<=4; m++){
+    let cuartos= document.getElementById(`llaveCuartos${m}`);
+    cuartos.addEventListener('click',(e)=> {
+        if(e.target && e.target.tagName === 'INPUT'){
+            let semis= document.getElementById(`${e.target.name}`).children;
+            let img=semis[0];
+            let p =semis[1];
+            let input=semis[2]
+            input.setAttribute("value",e.target.value);
+            p.textContent=e.target.value;   
+        }
+    })
+
+}
+for(let m=1;m<=2; m++){
+    let cuartos= document.getElementById(`llaveSemis${m}`);
+    cuartos.addEventListener('click',(e)=> {
+        if(e.target && e.target.tagName === 'INPUT'){
+            let final= document.getElementById(`${e.target.name}`).children;
+            let img=final[0];
+            let p =final[1];
+            let input=final[2]
+            input.setAttribute("value",e.target.value);
+            p.textContent=e.target.value;   
+        }
+    })
+
+}
+let finalOG=document.getElementById('llaveFinal');
+finalOG.addEventListener('click',(e)=>{
+    if(e.target && e.target.tagName === 'INPUT'){
+        console.log(`EL GANADOR ES ${e.target.value}`)   
+    }
+})
 
